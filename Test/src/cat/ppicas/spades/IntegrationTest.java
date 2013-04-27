b96f9f95f62f83dba1810d24bf48dd47bbec063d
Pau Picas Sans <zovskaya@gmail.com>
2013-04-27 17:17:01 +0200

    Added a dummy test for check the environment

---
+++ Test/src/cat/ppicas/spades/IntegrationTest.java
@@ -0,0 +1,15 @@
+package cat.ppicas.spades;
+
+import junit.framework.TestCase;
+
+public class IntegrationTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testUnitTesting() throws Exception {
+		assertTrue(true);
+	}
+
+}
