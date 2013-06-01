cbecac5e090ff1e553152744bbf94a41c9a37a81
Pau Picas Sans <zovskaya@gmail.com>
2013-06-01 21:55:57 +0200

    Adding one to one test model.

---
+++ Test/src/cat/ppicas/spades/models/auto/Address.java
@@ -0,0 +1,46 @@
+package cat.ppicas.spades.models.auto;
+
+import cat.ppicas.spades.Entity;
+
+public class Address implements Entity {
+
+	private long mId;
+	private String mStreet;
+	private String mTown;
+	private String mPostalCode;
+
+	@Override
+	public long getEntityId() {
+		return mId;
+	}
+
+	@Override
+	public void setEntityId(long id) {
+		mId = id;
+	}
+
+	public String getStreet() {
+		return mStreet;
+	}
+
+	public void setStreet(String street) {
+		mStreet = street;
+	}
+
+	public String getTown() {
+		return mTown;
+	}
+
+	public void setTown(String town) {
+		mTown = town;
+	}
+
+	public String getPostalCode() {
+		return mPostalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		mPostalCode = postalCode;
+	}
+
+}
