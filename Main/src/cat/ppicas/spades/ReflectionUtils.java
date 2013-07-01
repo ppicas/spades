6ee1103e4ed49e512eaed6362cc480105204b1af
Pau Picas Sans <ppicas@identitat.com>
2013-07-01 11:24:56 +0200

    Adding TableBuilder to refactor some functionality of Table
    * Adding methods in ReflectionUtils for find the corresponding entity
    class fields for table columns.

--- Main/src/cat/ppicas/spades/ReflectionUtils.java
+++ Main/src/cat/ppicas/spades/ReflectionUtils.java
@@ -17,4 +17,27 @@ public class ReflectionUtils {
 		throw new NoSuchFieldException(fieldName);
 	}
 
+	public static Field findFields(Class<?> cls, String[] fieldNames) {
+		return null;
+	}
+
+	/**
+	 * Generate a list of possible entity field names from a table column name.
+	 *
+	 * <p>
+	 * For example with the table column named {@code underscored_words} this
+	 * will return the following list:
+	 * <ul>
+	 * <li>camleCase</li>
+	 * <li>mCamleCase</li>
+	 * </ul>
+	 * </p>
+	 *
+	 * @param columnName The name of a table column.
+	 * @return An array of strings with the possible names.
+	 */
+	public static String[] generateFieldNamesFromColumnName(String columnName) {
+		return null;
+	}
+
 }
