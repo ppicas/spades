f67c9f0ab14b03d9cf7626bc7c2a6548908ba024
Pau Picas Sans <pau.picas@gmail.com>
2013-07-13 16:35:25 +0200

    Adding TableBuilder to refactor some functionality of Table

--- Main/src/cat/ppicas/spades/text/TextUtils.java
+++ Main/src/cat/ppicas/spades/text/TextUtils.java
@@ -1,5 +1,7 @@
 package cat.ppicas.spades.text;
 
+import static android.text.TextUtils.join;
+
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
@@ -23,46 +25,81 @@ public class TextUtils {
 	}
 
 	/**
-	 * Generate a list of possible entity field names from a table column name.
+	 * Generate a list of possible entity field names from the words of a table
+	 * column name.
 	 *
 	 * <p>
-	 * For example with the table column named {@code two_words} this
-	 * will return the following list:
+	 * For example if we have a column name {@code two_words} then this method
+	 * will receive the following words array: {@code two} and {@code words}.
+	 * Then with the previous array it will generate the following list:
 	 * <ul>
-	 * <li>two_words</li>
-	 * <li>twoWords</li>
 	 * <li>mTwoWords</li>
+	 * <li>twoWords</li>
+	 * <li>TwoWords</li>
+	 * <li>twowords</li>
+	 * <li>two_words</li>
+	 * <li>Two_Words</li>
+	 * <li>TWO_WORDS</li>
 	 * </ul>
 	 * </p>
 	 *
-	 * @param words The name of a table column.
+	 * @param words An array of words.
 	 * @return An array of strings with the possible names.
 	 */
 	public static Set<String> generateFieldNames(String[] words) {
 		Set<String> names = new HashSet<String>();
+
+		String[] capitalizedWords = capitalize(words);
+		String capitalized = concat(capitalizedWords);
+
+		String[] lowerCasedWords = lowerCase(words);
+		String lowerCased = concat(lowerCasedWords);
+
+		names.add("m" + capitalized);
+		names.add(firstCharToLowerCase(capitalized));
+		names.add(capitalized);
+		names.add(lowerCased);
+		names.add(join("_", lowerCasedWords));
+		names.add(join("_", capitalizedWords));
+		names.add(join("_", lowerCasedWords).toUpperCase(Locale.US));
+
 		return names;
 	}
 
-	/*private static String toCamelCase(String text) {
-		text = text.toLowerCase(Locale.US);
-		StringBuilder result = new StringBuilder();
-		for (String word : text.split("_")) {
-			result.append(capitalize(word));
+
+	private static String concat(String[] words) {
+		StringBuffer buffer = new StringBuffer();
+		for (String word : words) {
+			buffer.append(word);
 		}
-		return result.toString();
+		return buffer.toString();
 	}
 
-	private static String capitalize(String text) {
-		return text.substring(0, 1).toUpperCase(Locale.US)
-				+ text.substring(1).toLowerCase(Locale.US);
+	private static String[] capitalize(String... words) {
+		String[] capitalized = new String[words.length];
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];
+			capitalized[i] = capitalize(word);
+		}
+		return capitalized;
+	}
+
+	private static String capitalize(String word) {
+		return word.substring(0, 1).toUpperCase(Locale.US)
+				+ word.substring(1).toLowerCase(Locale.US);
+	}
+
+	private static String[] lowerCase(String[] words) {
+		String[] lowerCased = new String[words.length];
+		for (int i = 0; i < words.length; i++) {
+			String word = words[i];
+			lowerCased[i] = word.toLowerCase(Locale.US);
+		}
+		return lowerCased;
 	}
 
 	private static String firstCharToLowerCase(String text) {
 		return text.substring(0, 1).toLowerCase(Locale.US) + text.substring(1);
 	}
-
-	private static String firstCharToUpperCase(String text) {
-		return text.substring(0, 1).toUpperCase(Locale.US) + text.substring(1);
-	}*/
 
 }
--- Test/src/cat/ppicas/spades/text/TextUtilsTest.java
+++ Test/src/cat/ppicas/spades/text/TextUtilsTest.java
@@ -1,5 +1,8 @@
 package cat.ppicas.spades.text;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
 import java.util.Set;
 
 import junit.framework.TestCase;
@@ -7,97 +10,91 @@ import junit.framework.TestCase;
 public class TextUtilsTest extends TestCase {
 
 	public void test__Should_generate_field_names_from_column_name__Case_1() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("TWO_WORDS")
-				.words("two", "words")
-				.test();
+		new SplitIndentifierIntoWordsTest().identifier("TWO_WORDS").words("two", "words").test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_2() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("NOW_Three_Words")
-				.words("now", "three", "words")
-				.test();
+		new SplitIndentifierIntoWordsTest().identifier("NOW_Three_Words")
+				.words("now", "three", "words").test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_3() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("camelCasedWords")
-				.words("camel", "cased", "words")
-				.test();
+		new SplitIndentifierIntoWordsTest().identifier("camelCasedWords")
+				.words("camel", "cased", "words").test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_4() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("CamelCasedWords")
-				.words("camel", "cased", "words")
-				.test();
+		new SplitIndentifierIntoWordsTest().identifier("CamelCasedWords")
+				.words("camel", "cased", "words").test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_5() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("HTMLContent")
-				.words("html", "content")
+		new SplitIndentifierIntoWordsTest().identifier("HTMLContent").words("html", "content")
 				.test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_6() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("contentHTML")
-				.words("content", "html")
+		new SplitIndentifierIntoWordsTest().identifier("contentHTML").words("content", "html")
 				.test();
 	}
 
 	public void test__Should_generate_field_names_from_column_name__Case_7() throws Exception {
-		new SplitIndentifierIntoWordsTest()
-				.identifier("mixed_underscored_andCamelCased")
-				.words("mixed", "underscored", "andcamelcased")
-				.test();
+		new SplitIndentifierIntoWordsTest().identifier("mixed_underscored_andCamelCased")
+				.words("mixed", "underscored", "andcamelcased").test();
 	}
 
 	public void test__Should_generate_field_names_from_words__Case_1() throws Exception {
-		String[] words = new String[] { "word" };
-		Set<String> names = TextUtils.generateFieldNames(words);
-		assertTrue(names.contains("word"));
-		assertTrue(names.contains("mWord"));
+		String[] words = givenWords("word");
+		Set<String> fieldNames = TextUtils.generateFieldNames(words);
+		assertThat(fieldNames, containsInAnyOrder("mWord", "word", "Word", "WORD"));
 	}
 
-	/*public void test__Should_generate_field_names_from_column_name__When_one_word() throws Exception {
-		Set<String> names = ReflectionUtils.generateFieldNamesFromColumnName("one");
-		assertTrue(names.contains("one"));
-		assertTrue(names.contains("mOne"));
-	}
+	/*
+	 * public void
+	 * test__Should_generate_field_names_from_column_name__When_one_word()
+	 * throws Exception { Set<String> names =
+	 * ReflectionUtils.generateFieldNamesFromColumnName("one");
+	 * assertTrue(names.contains("one")); assertTrue(names.contains("mOne")); }
+	 *
+	 * public void
+	 * test__Should_generate_field_names_from_column_name__When_two_words()
+	 * throws Exception { Set<String> names =
+	 * ReflectionUtils.generateFieldNamesFromColumnName("TWO_WORDS");
+	 * assertTrue(names.contains("two_words"));
+	 * assertTrue(names.contains("twoWords"));
+	 * assertTrue(names.contains("mTwoWords")); }
+	 *
+	 * public void
+	 * test__Should_generate_field_names_from_column_name__When_two_words_capitalized
+	 * () throws Exception { Set<String> names =
+	 * ReflectionUtils.generateFieldNamesFromColumnName("twoWords");
+	 * assertTrue(names.contains("twoWords"));
+	 * assertTrue(names.contains("mTwoWords")); }
+	 *
+	 * public void
+	 * test__Should_generate_field_names_from_column_name__When_has_prefix_is()
+	 * throws Exception { Set<String> names =
+	 * ReflectionUtils.generateFieldNamesFromColumnName("is_boolean");
+	 * assertTrue(names.contains("is_boolean"));
+	 * assertTrue(names.contains("isBoolean"));
+	 * assertTrue(names.contains("mIsBoolean"));
+	 * assertTrue(names.contains("boolean"));
+	 * assertTrue(names.contains("mBoolean")); }
+	 *
+	 * public void
+	 * test__Should_generate_field_names_from_column_name__When_has_suffix_id()
+	 * throws Exception { Set<String> names =
+	 * ReflectionUtils.generateFieldNamesFromColumnName("something_id");
+	 * assertTrue(names.contains("something_id"));
+	 * assertTrue(names.contains("somethingId"));
+	 * assertTrue(names.contains("mSomethingId"));
+	 * assertTrue(names.contains("something"));
+	 * assertTrue(names.contains("mSomething")); }
+	 */
 
-	public void test__Should_generate_field_names_from_column_name__When_two_words() throws Exception {
-		Set<String> names = ReflectionUtils.generateFieldNamesFromColumnName("TWO_WORDS");
-		assertTrue(names.contains("two_words"));
-		assertTrue(names.contains("twoWords"));
-		assertTrue(names.contains("mTwoWords"));
+	private String[] givenWords(String... words) {
+		return words;
 	}
-
-	public void test__Should_generate_field_names_from_column_name__When_two_words_capitalized() throws Exception {
-		Set<String> names = ReflectionUtils.generateFieldNamesFromColumnName("twoWords");
-		assertTrue(names.contains("twoWords"));
-		assertTrue(names.contains("mTwoWords"));
-	}
-
-	public void test__Should_generate_field_names_from_column_name__When_has_prefix_is() throws Exception {
-		Set<String> names = ReflectionUtils.generateFieldNamesFromColumnName("is_boolean");
-		assertTrue(names.contains("is_boolean"));
-		assertTrue(names.contains("isBoolean"));
-		assertTrue(names.contains("mIsBoolean"));
-		assertTrue(names.contains("boolean"));
-		assertTrue(names.contains("mBoolean"));
-	}
-
-	public void test__Should_generate_field_names_from_column_name__When_has_suffix_id() throws Exception {
-		Set<String> names = ReflectionUtils.generateFieldNamesFromColumnName("something_id");
-		assertTrue(names.contains("something_id"));
-		assertTrue(names.contains("somethingId"));
-		assertTrue(names.contains("mSomethingId"));
-		assertTrue(names.contains("something"));
-		assertTrue(names.contains("mSomething"));
-	}*/
 
 	private class SplitIndentifierIntoWordsTest {
 		private String mIdentifier;
