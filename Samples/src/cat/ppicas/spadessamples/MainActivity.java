891b99d7ed1b4b8b210cd7728fe6f68382eaa034
Pau Picas Sans <pau.picas@gmail.com>
2013-10-08 23:26:14 +0200

    Created sample activities launcher

--- Samples/src/cat/ppicas/spadessamples/MainActivity.java
+++ Samples/src/cat/ppicas/spadessamples/MainActivity.java
@@ -1,7 +1,10 @@
 package cat.ppicas.spadessamples;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		findViewById(R.id.cursor_list).setOnClickListener(new ClickListener(MainActivity.class));
+		findViewById(R.id.object_list).setOnClickListener(new ClickListener(MainActivity.class));
+	}
+
+	private class ClickListener implements OnClickListener {
+		private Class<?> mActivity;
+
+		public ClickListener(Class<?> activity) {
+			mActivity = activity;
+		}
+
+		@Override
+		public void onClick(View v) {
+			startActivity(new Intent(MainActivity.this, mActivity));
+		}
 	}
 
 }
