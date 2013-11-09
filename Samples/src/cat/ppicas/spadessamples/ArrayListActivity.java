f44c6172fc53160739564b593e849af708571c66
Pau Picas Sans <pau.picas@gmail.com>
2013-11-09 20:28:34 +0100

    Implementing array list sample

--- Samples/src/cat/ppicas/spadessamples/ArrayListActivity.java
+++ Samples/src/cat/ppicas/spadessamples/ArrayListActivity.java
@@ -1,9 +1,9 @@
 package cat.ppicas.spadessamples;
 
+import java.util.List;
+
 import android.app.ListActivity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,14 +11,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import cat.ppicas.spades.query.Query;
-import cat.ppicas.spadessamples.adapter.PersonCursorAdapter;
 import cat.ppicas.spadessamples.model.ContactPointDao;
 import cat.ppicas.spadessamples.model.DatabaseHelper;
+import cat.ppicas.spadessamples.model.Person;
 import cat.ppicas.spadessamples.model.PersonDao;
 
 public class ArrayListActivity extends ListActivity {
-
-	private Cursor mCursor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +24,17 @@ public class ArrayListActivity extends ListActivity {
 
 		DatabaseHelper helper = new DatabaseHelper(this);
 		SQLiteDatabase db = helper.getReadableDatabase();
+		PersonDao dao = new PersonDao(db);
 
 		Query query = new Query(PersonDao.TABLE)
 				.leftJoin(ContactPointDao.TABLE, "%s = %s", ContactPointDao.PERSON_ID, PersonDao.ID)
 				.groupBy(PersonDao.ID);
-		mCursor = query.execute(db);
-		DatabaseUtils.dumpCursor(mCursor);
+		List<Person> people = dao.fetchAllWithRelated(query);
 
 		helper.close();
 
-		setListAdapter(new PersonCursorAdapter(this, mCursor, query.getCursorInfo()));
+		// setListAdapter(new PersonCursorAdapter(this, mCursor, query.getCursorInfo()));
 		getListView().setOnItemClickListener(mListItemClickListener );
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-
-		if (mCursor != null) {
-			mCursor.close();
-		}
 	}
 
 	private final OnItemClickListener mListItemClickListener = new OnItemClickListener() {
